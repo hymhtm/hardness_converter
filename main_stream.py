eb3aa36c4f9312e7b9ff807c746bbd4fe6f8d58c
hymhtm <hymhtm@gmail.com>
2025-01-22 13:01:40 +0900

    Refactor main_stream.py to update page icon, enhance UI with buttons for calculation and clearing results, and improve result display. Remove pre_build_script.py as it is no longer needed. Update requirements.txt to include pandas and streamlit.

--- main_stream.py
+++ main_stream.py
@@ -10,7 +10,7 @@ def main():
     ui_logging.log_info("launching application")
     st.set_page_config(
         page_title="Hardness Converter",
-        page_icon=":material/calculate:",
+        page_icon="logo.png",
         layout="centered",
         initial_sidebar_state="auto",
         menu_items=None,
@@ -18,10 +18,18 @@ def main():
     
     st.title("熱処理硬度 換算",anchor=False,)
     hardness_input = st.text_input("硬度を入力してください")
-    ui_logging.log_info("hardness input field created")
-    source_scale = st.selectbox("硬度の種類を選択してください",options=scale_list, index=0)
-    ui_logging.log_info("source index field created")
+    source_scale = st.selectbox("硬度の種類を選択してください",options=scale_list, index=None)   
     
+    col1, col2, _, _, _, _ = st.columns(6, gap='small')
+    with col1:
+        st.button("換算する", on_click=lambda: calculate(hardness_input, source_scale))
+    with col2:
+        st.button("クリア", on_click=lambda: result_container.empty())
+
+    result_container = st.container()
+    with result_container:
+        HRC, HV, HBW, HS = st.columns(4, gap="medium")
+        
     def calculate(hardness_input, source_scale):
         ui_logging.log_info("calculate button clicked" )
         result_container.empty()
@@ -31,22 +39,15 @@ def main():
             converted_values = converter.convert_hardness_scale(hardness_input, source_index)
             with result_container:
                 if converted_values:
-                    st.dataframe(converted_values)
+                    HRC.metric(label="HRC", value= converted_values["HRC"] if converted_values["HRC"] is not np.nan else "❌", delta_color="normal",border=True)
+                    HV.metric(label="HV", value=converted_values["HV"] if converted_values["HV"] is not np.nan else "❌", delta_color="normal",border=True)
+                    HBW.metric(label="HBW", value=converted_values["HBW"] if converted_values["HBW"] is not np.nan else "❌", delta_color="normal",border=True)
+                    HS.metric(label="HS", value=converted_values["HS"] if converted_values["HS"] is not np.nan else "❌", delta_color="normal",border=True)
                 else:
-                    st.write("No data to display")
+                    st.write("データがありません")
         except Exception as e:
             ui_logging.log_exception(e)
-            st.write("An error occurred while calculating. Please try again.")
-    
-    col1, col2, _, _, _ = st.columns(5, gap='small')
-    with col1:
-        st.button("Calculate", on_click=lambda: calculate(hardness_input, source_scale))
-    with col2:
-        st.button("Clear", on_click=lambda: st.rerun())
-    result_container = st.container()
-    
-    ui_logging.log_info("calculate button created")
-    ui_logging.log_info("result container created")
-    ui_logging.log_info("application launched")
+            st.write("換算に失敗しました。もう一度お試しください。")
+        
 if __name__ == "__main__":
     main()
